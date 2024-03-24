309b791e83c027ae306ce3cc6fe251009d1aae66
Yamani <tasubilliyamani@gmail.com>
2024-03-24 21:15:18 +0530

    props

---
+++ props/src/components/product.jsx
@@ -0,0 +1,14 @@
+function Product ({name,price,brand}) {
+
+    return(
+        <>
+        <h1>Name : {name}</h1>
+        <h2>Price : {price}</h2>
+        <h2>Brand : {brand}</h2>
+        <br/>
+        <br/>
+        </>
+    )
+}
+
+export default Product
